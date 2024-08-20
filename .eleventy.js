474643b68e820125bc6297b53100ed16ea25b555
hamzabawumia <hamzabawumia@yahoo.com>
2024-08-20 06:47:36 -0700

    fixing issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
   return {
     dir: {
-      output: "build"
+      input: "learn_french_in_45_days", // This is your source directory
+      output: "learn_french_in_45_days/_site" // This is where Eleventy will build the site
     }
   };
 };
