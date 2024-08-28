128d9dd9c4874650bd963e170f65035488cc70d4
hamzabawumia <hamzabawumia@yahoo.com>
2024-08-28 09:41:36 -0700

    improved

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
 module.exports = function(eleventyConfig) {
+  // Define input and output directories
+  eleventyConfig.addPassthroughCopy("learn_french_in_45_days/_includes"); // Optional: Copy static assets, if any
+  
+  // Add global data
+  eleventyConfig.addGlobalData('baseUrl', '/');
+  // now i can specigy the baseUrl in my code as {{ baseUrl }}
+  
+  // Add other configurations if needed
+
+  // Return directory settings
   return {
     dir: {
-      input: "learn_french_in_45_days", // This is your source directory
-      output: "learn_french_in_45_days/_site" // This is where Eleventy will build the site
+      input: "learn_french_in_45_days", // Source directory
+      output: "learn_french_in_45_days/_site" // Build output directory
     }
   };
 };
