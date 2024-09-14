d934aed510a9880624c775e000b55c58b346ee33
hamzabawumia <hamzabawumia@yahoo.com>
2024-09-14 16:46:03 -0700

    Thank God for the victory

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function(eleventyConfig) {
   // Define input and output directories
-  eleventyConfig.addPassthroughCopy("learn_french_in_45_days/_includes"); // This is important for git-pages to copy the static files
+  eleventyConfig.addPassthroughCopy("french_in_45_days/_includes"); // This is important for git-pages to copy the static files
 
   eleventyConfig.setTemplateFormats(["njk", "html", "md"]);
   // Return directory settings
   return {
     dir: {
-      input: "learn_french_in_45_days", // Source directory
-      output: "learn_french_in_45_days/_site", // Build output directory
+      input: "french_in_45_days", // Source directory
+      output: "french_in_45_days/_site", // Build output directory
       includes: "_includes", // Directory for includes (partials, templates, etc.)
     }
   };
